d2153a6def992486265951fa9f5ce9e7039890c5
Amir Mahdi Gholipour <amirfgh89@gmail.com>
2026-08-14 17:56:38 +0330

    create cafe creation view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from serializers import CafeSerializer
 
-# Create your views here.
+
+class CafeView(APIView):
+    def post(self, request):
+        serializer = CafeSerializer(data=request.data)
