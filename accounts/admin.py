0da2e53b4f916035416596b3f97d337f64f14579
Amir Mahdi Gholipour <amirfgh89@gmail.com>
2026-08-15 02:14:37 +0330

    register models fix logo preview

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CafeModel
+from django.utils.html import format_html
 
-# Register your models here.
+
+@admin.register(CafeModel)
+class CafeModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'logo_preview']
+    readonly_fields = ['logo_preview']
+
+    def logo_preview(self, obj):
+        if obj.logo:
+            return format_html('<img src="{}" width="100" height="100" />', obj.logo.url)
+        return "No logo available"
