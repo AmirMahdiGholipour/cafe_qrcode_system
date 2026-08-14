9518df78b11ac2074d46db64a707fbfdf3d25266
Amir Mahdi Gholipour <amirfgh89@gmail.com>
2026-08-14 18:03:10 +0330

    update view.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework import status
 from serializers import CafeSerializer
 
 
 class CafeView(APIView):
+    permission_classes = [AllowAny]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
+
     def post(self, request):
         serializer = CafeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
