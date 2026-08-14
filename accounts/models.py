80c9c62c37bbee7a257504ef090addc6bc44f41e
Amir Mahdi Gholipour <amirfgh89@gmail.com>
2026-08-14 17:56:07 +0330

    complete cafe model

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,20 @@
 from django.db import models
-from rest_framework.validators import rege
+from django.core.validators import RegexValidator
 
 class CafeModel(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    logo = models.ImageField(upload_to='cafe_logo', null=True, blank=True)
+    logo = models.ImageField(upload_to='cafes/logos', null=True, blank=True)
+    cover_image = models.ImageField(upload_to='cafes/covers', null=True, blank=True)
+    primary_color = models.CharField(
+        max_length=7,
+        validators=[
+            RegexValidator(
+                regex=r'^#(?:[0-9a-fA-F]{3}){1,2}$',
+                message='Primary color must be a hex color code',
+            )
+        ]
+    )
+
+    def __str__(self):
+        return self.name
