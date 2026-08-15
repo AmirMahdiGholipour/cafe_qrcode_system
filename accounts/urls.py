4c622e49d7cfeac03cfedd3b61ee3e64b5ace678
Amir Mahdi Gholipour <amirfgh89@gmail.com>
2026-08-15 16:06:22 +0330

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
+# accounts/urls.py
 from django.urls import path
-from .views import CafeAddView, CafeStaffAddView
+from .views import SignupView, CafeAddView, StaffAddView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
 urlpatterns = [
-    path('cafe/add/', CafeAddView.as_view(), name='cafe-add'),
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('staff/add/', CafeStaffAddView.as_view(), name='cafe-staff-add'),
+    path("auth/signup/", SignupView.as_view(), name="signup"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("cafe/add/", CafeAddView.as_view(), name="cafe-add"),
+    path("cafes/<int:cafe_id>/staff/add/", StaffAddView.as_view(), name="staff-add"),
 ]
