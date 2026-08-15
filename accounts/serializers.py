bdd1403647bb6dd3099268b0348b369a2f119dcc
Amir Mahdi Gholipour <amirfgh89@gmail.com>
2026-08-15 15:55:48 +0330

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CafeModel, CafeStaffModel
+from .models import CafeModel, CafeStaffModel, CafeMembership
 
 
 class CafeSerializer(serializers.ModelSerializer):
@@ -9,18 +9,25 @@ class CafeSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'slug')
 
 
-class CafeBrandingSerializer(serializers.ModelSerializer):
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
     class Meta:
         model = CafeModel
-        fields = ['name', 'logo', 'cover_image', 'primary_color']
-
-
-class CafeStaffSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
-    class Meta:
-        model = CafeStaffModel
-        fields = ['id', 'username', 'password','first_name', 'last_name', 'role', 'cafe']
-        read_only_fields = ('id',)
+        fields = [
+            'id', 'username', 'password', 'first_name', 'last_name',
+            'phone_number', 'national_code', 'address', 'province', 'city', 'gender',
+        ]
+        extra_kwargs = {
+            "first_name": {'required': True},
+            "last_name": {'required': True},
+            "phone_number": {'required': True},
+            "national_code": {'required': True},
+            "address": {'required': True},
+            "province": {'required': True},
+            "city": {'required': True},
+            "gender": {'required': True},
+        }
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -28,3 +35,44 @@ class CafeStaffSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+class StaffCreateSerializer(SignupSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    role = serializers.ChoiceField(choices=CafeMembership.StaffRole.choices)
+
+    class Meta:
+        model = CafeStaffModel
+        fields = ['id', 'username', 'password', 'first_name', 'last_name', 'role', 'gender']
+
+    def validate_role(self, value):
+        creator = self.context['request'].user
+        creator_role = self.context['creator_role']
+
+        allowed = {
+            CafeMembership.StaffRole.MANAGER: {CafeMembership.StaffRole.ADMIN, CafeMembership.StaffRole.STAFF},
+            CafeMembership.StaffRole.ADMIN: {CafeMembership.StaffRole.STAFF},
+        }
+
+        if value not in allowed.get(creator_role, set()):
+            raise serializers.ValidationError("Role must be one of {}".format(allowed.get(creator_role)))
+        return value
+
+    def create(self, validated_data):
+        role = validated_data.pop('role')
+        password = validated_data.pop('password')
+        cafe = self.context['cafe']
+
+        user = CafeStaffModel(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        CafeMembership.objects.create(user=user, cafe=cafe, role=role)
+        return user
+
+
+class CafeMembershipSerializer(serializers.ModelSerializer):
+    cafe_name = serializers.CharField(source="cafe.name", read_only=True)
+
+    class Meta:
+        model = CafeMembership
+        fields = ["id", "cafe", "cafe_name", "role", "created_at"]
